b2429eb053041b88b53c950c6b873e8b6013c806
selbin <selbinzr@gmail.com>
2023-09-16 20:51:36 +0530

    updated cors

--- server/src/index.js
+++ server/src/index.js
@@ -18,12 +18,7 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
